4a229d338d6fa42105a5ddef59ae95350fbf02c5
harun <harun@traveloka.com>
2018-06-07 12:22:38 +0700

    fix extract() to handle flexible params

--- all-about-that-bash.sh
+++ all-about-that-bash.sh
@@ -824,18 +824,18 @@ keyboard-map() {
 extract () {
    if [ -f $1 ] ; then
        case $1 in
-           *.tar.bz2)   tar xvjf $1    ;;
-           *.tar.gz)    tar xvzf $1    ;;
-           *.bz2)       bunzip2 $1     ;;
-           *.rar)       unrar x $1     ;;
-           *.gz)        gunzip $1      ;;
-           *.tar)       tar xvf $1     ;;
-           *.tbz2)      tar xvjf $1    ;;
-           *.tgz)       tar xvzf $1    ;;
-           *.zip)       unzip $1       ;;
-           *.Z)         uncompress $1  ;;
-           *.7z)        7z x $1        ;;
-           *)           echo "don't know how to extract '$1'..." ;;
+           *.tar.bz2)   tar xvjf $@    ;;
+           *.tar.gz)    tar xvzf $@    ;;
+           *.bz2)       bunzip2 $@     ;;
+           *.rar)       unrar x $@     ;;
+           *.gz)        gunzip $@      ;;
+           *.tar)       tar xvf $@     ;;
+           *.tbz2)      tar xvjf $@    ;;
+           *.tgz)       tar xvzf $@    ;;
+           *.zip)       unzip $@       ;;
+           *.Z)         uncompress $@  ;;
+           *.7z)        7z x $@        ;;
+           *)           echo "don't know how to extract '$@'..." ;;
        esac
    else
        echo "'$1' is not a valid file!"
